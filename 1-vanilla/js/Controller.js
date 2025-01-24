fae7588525e296b63fdc478c2a08e267d1276903
beomjo <dongjun1315@naver.com>
2025-01-24 17:29:34 +0900

    feat : 검색 결과 호출

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -3,8 +3,19 @@ const tag = "[Controller]";
 export default class Controller {
   constructor(store, { searchFormView }) {
     console.log(tag);
+
     this.store = store;
 
     this.searchFormView = searchFormView;
+
+    this.subscribeViewEvents();
+  }
+
+  subscribeViewEvents() {
+    this.searchFormView.on("@submit", (event) => this.search(event));
+  }
+
+  search(event) {
+    console.log(tag, event, event.detail.value);
   }
 }
--- 1-vanilla/js/views/SearchFromView.js
+++ 1-vanilla/js/views/SearchFromView.js
@@ -22,11 +22,19 @@ export default class SearchFormView extends View{
 
     bindEvent(){
         on(this.inputElement, "keyup", () => this.handleKeyup());
+        on(this.element, "submit", event => this.handleSubmit(event));
     }
 
     handleKeyup(){
         console.log(tag, "handleKeyup", this.inputElement.value);
         const {value} = this.inputElement;
         this.showResetButton(value.length > 0);    
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(tag, 'handleSubmit');
+        const { value } = this.inputElement;
+        this.emit("@submit", {value}); // custom event
     }
 }
