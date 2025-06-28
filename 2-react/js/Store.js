365ec5ae6ec59dc1d4a792c010d23e2b053c6021
beomjo <dongjun1315@naver.com>
2025-06-28 16:47:49 +0900

    feat: 최근 검색어 구현

--- 2-react/js/Store.js
+++ 2-react/js/Store.js
@@ -1,3 +1,4 @@
+import { createNextId } from './helpers.js';
 import storage from './storage.js';
 
 class Store {
@@ -9,13 +10,48 @@ class Store {
   }
 
   search(keyword) {
+    this.addHistory(keyword);
+
     return this.storage.productData.filter((product) =>
-        product.name.includes(keyword)
+      product.name.includes(keyword)
     );
   }
 
   getKeywordList() {
-    return this.storage.keywordData;
+    return [...this.storage.keywordData];
+  }
+
+  getHistoryList() {
+    return [...this.storage.historyData.sort(this._sortHistory)];
+  }
+
+  addHistory(keyword = '') {
+    keyword = keyword.trim();
+    if (!keyword) {
+      return;
+    }
+    
+    const hasHistory = this.storage.historyData.some(
+      (history) => history.keyword === keyword
+    );
+    if (hasHistory) this.removeHistory(keyword);
+
+    const id = createNextId(this.storage.historyData);
+    const date = new Date();
+    this.storage.historyData.push({ id, keyword, date });
+    console.log(this.storage.historyData);
+    this.storage.historyData = [...this.storage.historyData.sort(this._sortHistory)];
+  }
+
+  /* 최근 검색어 내림차순으로 정렬 -> 날짜의 역순 */
+  _sortHistory(history1, history2) {
+    return history2.date - history1.date;
+  }
+
+  removeHistory(keyword) {
+    return this.storage.historyData = this.storage.historyData.filter(
+      (history) => history.keyword !== keyword
+    )
   }
 }
 
