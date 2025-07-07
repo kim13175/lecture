a85243c9ae73c31fda305fdad89c4154e8a0afe5
beomjo <dongjun1315@naver.com>
2025-07-07 14:02:29 +0900

    feat: 검색 폼 컴포넌트 구현

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,11 +1,42 @@
 import React from "react";
 import Header from "./components/Header";
+import SearchForm from "./components/SearchForm";
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = { searchKeyword: "" }
+    }
+
+    handleChangeInput(searchKeyword) {
+        if (searchKeyword.length <= 0) {
+            this.handleReset();
+        }
+
+        this.setState({ searchKeyword });
+    }
+
+    search(searchKeyword) {
+        console.log(searchKeyword);
+    }
+
+    handleReset() {
+        console.log('reset');
+    }
+
     render() {
         return (
             <>
                 <Header title="검색"/>
+                <div className="container">
+                    <SearchForm
+                        value={this.state.searchKeyword}
+                        onChange={(value) => this.handleChangeInput(value)} 
+                        onSubmit={(searchKeyword) => this.search(searchKeyword)} 
+                        onReset={() => this.handleReset()}
+                    />
+                </div>
             </>
         )
     }
